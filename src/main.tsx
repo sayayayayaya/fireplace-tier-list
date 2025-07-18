10718306a4f5ff5abfa5ea1b83421a12680d9421
Saya <sh5155@gmail.com>
2025-07-18 20:39:55 +0800

    Cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router'
 import './index.css'
-import App from './App.tsx'
 import TierList from './TierList.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<TierList />} />
         <Route path="/tier-list" element={<TierList />} />
       </Routes>
     </BrowserRouter>
